d8bbeb6bfc1b25fa18b33639ecd1fe4916a22a38
Adi Rosadi <adirosadi49@gmail.com>
2022-10-06 22:06:20 +0700

    fungsi get

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import express from 'express';
 const app = express()
 
 app.get('/',(request, response) =>{
-    console.log(request.query)
-    response.json({title:'HALLO'})
+   // console.log(request.query.name)
+    response.json({title:`HALLO ${request.query.name}`})
 })
 
 app.listen(3000,() => {
